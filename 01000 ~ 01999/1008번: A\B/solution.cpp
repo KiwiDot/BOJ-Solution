e2aa1f5fafcb246c58fe51ffa743deacbfc7d59b
KiwiDot <116772632+KiwiDot@users.noreply.github.com>
2023-05-12 10:12:23 +0900

    [ C++17 ]
    2023-05-12

---
+++ 01000 ~ 01999/1008번: A\B/solution.cpp
@@ -0,0 +1,15 @@
+// 백준 1008번 A/B
+# include <iostream>
+using namespace std;
+
+int main() {
+	double A, B;
+	cout << fixed;
+	cout.precision(9);
+
+
+	cin >> A >> B;
+	cout << A / B << endl;
+
+	return 0;
+}
