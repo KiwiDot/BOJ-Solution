7d55ed0fe14806273c90faf896f9edf2edf4eb98
KiwiDot <116772632+KiwiDot@users.noreply.github.com>
2023-05-18 09:57:04 +0900

    [ C++17 ]
    2023-05-18

---
+++ 01000 ~ 01999/1330번: 두 수 비교하기/solution.cpp
@@ -0,0 +1,17 @@
+// 백준 1330번 두 수 비교하기
+# include <iostream>
+using namespace std;
+
+int main() {
+	int A, B;
+	cin >> A >> B;
+
+	if (A > B)
+		cout << ">" << endl;
+	else if (A < B)
+		cout << "<" << endl;
+	else
+		cout << "==" << endl;
+
+	return 0;
+}
