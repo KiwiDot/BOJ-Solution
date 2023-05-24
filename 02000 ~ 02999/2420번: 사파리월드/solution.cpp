cff6c893d3d7fcb92c19539226e3bd032994b0a2
KiwiDot <116772632+KiwiDot@users.noreply.github.com>
2023-05-24 11:27:51 +0900

    [ C++17 ]
    2023-05-24

---
+++ 02000 ~ 02999/2420번: 사파리월드/solution.cpp
@@ -0,0 +1,13 @@
+// 백준 2420번 사파리월드
+# include <iostream>
+# include <stdio.h>
+using namespace std;
+
+int main() {
+	long long N, M;
+	cin >> N >> M;
+
+	cout << llabs(N - M) << endl;
+
+	return 0;
+}
