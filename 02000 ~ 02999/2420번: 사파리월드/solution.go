0e0d7e1a722e824d5c0b18362363f31c7bbc8e5c
KiwiDot <116772632+KiwiDot@users.noreply.github.com>
2023-05-24 11:29:08 +0900

    [ GO ]
    2023-05-24

---
+++ 02000 ~ 02999/2420번: 사파리월드/solution.go
@@ -0,0 +1,17 @@
+// 백준 2420번 사파리월드
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var N, M int
+	fmt.Scanln(&N, &M)
+
+	if N > M {
+		fmt.Println(N - M)
+	} else{
+		fmt.Println(M - N)
+	}
+}
