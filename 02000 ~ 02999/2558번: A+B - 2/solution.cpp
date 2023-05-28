c63ce0ef4abee26403fa97a718dde9dae3ace2d1
KiwiDot <116772632+KiwiDot@users.noreply.github.com>
2023-05-28 13:09:50 +0900

    [ C++17 ]
    2023-05-28

---
+++ 02000 ~ 02999/2558번: A+B - 2/solution.cpp
@@ -0,0 +1,13 @@
+// 백준 2558번 A+B - 2
+# define _CRT_SECURE_NO_WARNINGS
+# include <stdio.h>
+
+int main() {
+	int A, B;
+
+	scanf("%d", &A);
+	scanf("%d", &B);
+	printf("%d", A + B);
+
+	return 0;
+}
