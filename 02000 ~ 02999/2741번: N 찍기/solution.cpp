88f85989db4074535d516a8d773064fbb787f07e
KiwiDot <116772632+KiwiDot@users.noreply.github.com>
2023-05-31 09:45:17 +0900

    [ C++17 ]
    2023-05-31

---
+++ 02000 ~ 02999/2741번: N 찍기/solution.cpp
@@ -0,0 +1,14 @@
+// 백준 2741번 N 찍기
+# include <iostream>
+using namespace std;
+
+int main() {
+	long long N;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		cout << i << "\n";
+	}
+
+	return 0;
+}
