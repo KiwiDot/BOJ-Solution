059b043416285993e62e024013ee97ab744b2b54
KiwiDot <116772632+KiwiDot@users.noreply.github.com>
2023-06-01 10:05:20 +0900

    [ C++17 ]
    2023-06-01

---
+++ 02000 ~ 02999/2743번: 단어 길이 재기/solution.cpp
@@ -0,0 +1,15 @@
+// 백준 2743번 단어 길이 재기
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+	char word[101];
+
+	scanf("%s", word);
+	printf("%d", strlen(word));
+
+	return 0;
+}
