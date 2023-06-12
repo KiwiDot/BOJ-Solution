ad81cb87fd82b5918aeaaa2de6c5da2035cdf847
KiwiDot <116772632+KiwiDot@users.noreply.github.com>
2023-06-12 09:35:51 +0900

    [ C++17 ]
    2023-06-12

---
+++ 05000 ~ 05999/5337번: 웰컴/solution.cpp
@@ -0,0 +1,13 @@
+// 백준 5337번 웰컴
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	printf(".  .   .\n");
+	printf("|  | _ | _. _ ._ _  _\n");
+	printf("|/\\|(/.|(_.(_)[ | )(/.\n");
+	
+	return 0;
+}
