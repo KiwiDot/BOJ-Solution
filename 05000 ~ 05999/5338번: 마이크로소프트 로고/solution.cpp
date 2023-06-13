74153f3200a0cd0cab44191fd5ad2c03b20f51a7
KiwiDot <116772632+KiwiDot@users.noreply.github.com>
2023-06-13 09:34:55 +0900

    [ C++17 ]
    2023-06-13

---
+++ 05000 ~ 05999/5338번: 마이크로소프트 로고/solution.cpp
@@ -0,0 +1,15 @@
+// 백준 5338번 마이크로소프트 로고
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	printf("       _.-;;-._\n");
+	printf("\'-..-\'|   ||   |\n");
+	printf("\'-..-\'|_.-;;-._|\n");
+	printf("\'-..-\'|   ||   |\n");
+	printf("\'-..-\'|_.-\'\'-._|\n");
+	
+	return 0;
+}
