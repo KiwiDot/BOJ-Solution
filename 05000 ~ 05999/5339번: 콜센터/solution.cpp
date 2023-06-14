41f7c373c2cc311f214c9a9953ba80e4716ce3ae
KiwiDot <116772632+KiwiDot@users.noreply.github.com>
2023-06-14 09:32:04 +0900

    [ C++17 ]
    2023-06-14

---
+++ 05000 ~ 05999/5339번: 콜센터/solution.cpp
@@ -0,0 +1,20 @@
+// 백준 5339번 콜센터
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	printf("     /~\\\n");
+	printf("    ( oo|\n");
+	printf("    _\\=/_\n");
+	printf("   /  _  \\\n");
+	printf("  //|/.\\|\\\\\n");
+	printf(" ||  \\ /  ||\n");
+	printf("============\n");
+	printf("|          |\n");
+	printf("|          |\n");
+	printf("|          |\n");
+	
+	return 0;
+}
