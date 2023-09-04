4702062be042035eea7969b05764758e010c29ed
KiwiDot <116772632+KiwiDot@users.noreply.github.com>
2023-09-04 12:45:19 +0900

    -
    2023-09-04

---
+++ 15000 ~ 15999/15992번: 1, 2, 3 더하기 7/boj15992.py
@@ -0,0 +1,20 @@
+# 백준 15992번 1, 2, 3 더하기 7
+import sys
+put = sys.stdin.readline
+
+T = int(put())
+dp = [[0] * 1001 for i in range(1001)]
+r = 1000000009
+
+dp[1] = [0, 1] + [0] * 9999
+dp[2] = [0, 1, 1] + [0] * 9998
+dp[3] = [0, 1, 2, 1] + [0] * 9997
+
+for i in range(4, 1001):
+    for j in range(1, 1001):
+        dp[i][j] = (dp[i-1][j-1] + dp[i-2][j-1] + dp[i-3][j-1]) % r
+
+while T:
+    T -= 1
+    n, m = map(int, put().split())
+    print(dp[n][m])
