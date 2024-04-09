b0231c94c2a93493827c45d6fb493dcf3bbfd4f0
KiwiDot <116772632+KiwiDot@users.noreply.github.com>
2024-04-09 10:26:52 +0900

    -
    2024-04-09

---
+++ 31000 ~ 31999/31673번: 특별한 학생회장 교체/boj31673.py
@@ -0,0 +1,17 @@
+# 백준 31673번 특별한 학생회장 교체
+import sys
+put = sys.stdin.readline
+
+N, M = map(int, put().split())
+V = sorted(list(map(int, put().split())), reverse=True)
+v = sum(V) / 2
+
+for i in range(N):
+    v -= V[i]
+    if v <= 0:
+        m = i + 2
+        break
+else:
+    m = N + 1
+
+print(M // m)
