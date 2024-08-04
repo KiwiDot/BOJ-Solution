45da6dc7684bf080bd29bc9a2fbf8926857be00c
KiwiDot <116772632+KiwiDot@users.noreply.github.com>
2024-08-04 13:23:37 +0900

    -
    2024-08-04

---
+++ 31000 ~ 31999/31248번: 3+1 하노이 탑/boj31248.py
@@ -0,0 +1,53 @@
+# 백준 31248번 3+1 하노이 탑
+import sys
+put = sys.stdin.readline
+loc_1 = 'A'
+
+
+def big_hanoi(n, T1, T2, T3, T4):
+    global loc_1
+    if n == 1:
+        print(loc_1, T4)
+        return
+
+    def little_hanoi(m, t1, t2, t3):
+        global loc_1
+        if m == 1:
+            print(t1, t3)
+            loc_1 = t3
+            return
+
+        little_hanoi(m-1, t1, t3, t2)
+        little_hanoi(1, t1, t2, t3)
+        little_hanoi(m-1, t2, t1, t3)
+
+    # (1) ~ (N-2)번째 원반까지 이동
+    if n > 2:
+        little_hanoi(n-2, T1, T2, T3)
+
+    # (N-1), (N)번째 원반 이동
+    print(T1, T2)
+    print(T1, T4)
+    print(T2, T4)
+
+    if n-2:
+        big_hanoi(n-2, T3, T1, T2, T4)
+
+
+N = n = int(put())
+answer = 0
+
+while n:
+    if n > 2:
+        answer += 2 ** (n-2) - 1
+        answer += 3
+    elif n == 2:
+        answer += 3
+    else:
+        answer += 1
+
+    n = max(n-2, 0)
+
+
+print(answer)
+big_hanoi(N, 'A', 'B', 'C', 'D')
